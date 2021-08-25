9b694de3ed7dd2a0a3e587fda3431f27674d4ac3
along8846 <ywl444@126.com>
2021-08-25 18:05:21 +0800

    1

--- deploy_to_dev.sh
+++ deploy_to_dev.sh
@@ -4,6 +4,8 @@ echo "APP_PROJECT_NAME2 is ${APP_PROJECT_NAME2}"
 echo "APP_PROJECT_NAME3 is ${APP_PROJECT_NAME3}"
 echo "APP_PROJECT_NAME4 is ${APP_PROJECT_NAME4}"
 echo "APP_PROJECT_NAME5 is ${APP_PROJECT_NAME5}"
+echo "GITLAB_REGISTRY is ${GITLAB_REGISTRY}"
+echo "GITHUB_REPOSITORY is ${GITHUB_REPOSITORY}"
 
 cd helm/${APP_PROJECT_NAME}/${APP_CHART_NAME}
 EXPECT_CHART=`echo ${GITHUB_REPOSITORY} | awk -F "/" '{print $2}'`
@@ -22,15 +24,15 @@ yq r --printMode pv values.yaml "common*.image.repository"
             
 echo "list current common*.image.tag"
 yq r --printMode pv values.yaml "common*.image.tag"
-echo "latest GITLAB_IMAGE_TAG is sha-${GITHUB_SHA:0:7}"            
-yq w -i values.yaml "common*.image.tag" sha-${GITHUB_SHA:0:7}
+echo "latest GITLAB_IMAGE_TAG is ${GITHUB_SHA:0:8}"            
+yq w -i values.yaml "common*.image.tag" ${GITHUB_SHA:0:8}
 yq r --printMode pv values.yaml "common*.image.tag"
 
-echo "push the latest SHA: ${GITHUB_SHA:0:7} to manifest repo"
+echo "push the latest SHA: ${GITHUB_SHA:0:8} to manifest repo"
 git config user.name ${GITHUB_ACTOR}
 git config user.email ${GITHUB_ACTOR}@github.com
 git diff
 git add ./
-git commit -m "${GITHUB_REPOSITORY}_${GITHUB_JOB}_${GITHUB_SHA:0:7}_details:${CI_COMMIT_MESSAGE}"
+git commit -m "${GITHUB_REPOSITORY}_${GITHUB_JOB}_${GITHUB_SHA:0:8}_details:${CI_COMMIT_MESSAGE}"
 git push
 echo -e "======================= \n\n you can check your application status with 'user/passwd:readonly/Te****g' at \n\n \033[31m https://g-argocd.fluxble.com/applications/${APP_PROJECT_NAME}-${APP_CHART_NAME}\033[0m \n\n======================="
