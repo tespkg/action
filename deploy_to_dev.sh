660b7546092d8206389fc53359e7b4af0773f265
along8846 <ywl444@126.com>
2021-08-26 15:58:18 +0800

    test

--- deploy_to_dev.sh
+++ deploy_to_dev.sh
@@ -4,11 +4,10 @@ echo "APP_PROJECT_NAME2 is ${APP_PROJECT_NAME2}"
 echo "APP_PROJECT_NAME3 is ${APP_PROJECT_NAME3}"
 echo "APP_PROJECT_NAME4 is ${APP_PROJECT_NAME4}"
 echo "APP_PROJECT_NAME5 is ${APP_PROJECT_NAME5}"
-echo "GITLAB_REGISTRY is ${GITLAB_REGISTRY}"
-echo "GITHUB_REPOSITORY is ${GITHUB_REPOSITORY}"
-echo "COMMON2 is ${COMMON2}"
-echo "COMMON3 is ${COMMON3}"
-echo "COMMON4 is ${COMMON4}"
+# echo "GITLAB_REGISTRY is ${GITLAB_REGISTRY}"
+# echo "GITHUB_REPOSITORY is ${GITHUB_REPOSITORY}"
+echo "SECOND_MODULE is ${SECOND_MODULE}"
+echo "THIRD_MODULE is ${THIRD_MODULE}"
 
 cd helm/${APP_PROJECT_NAME}/${APP_CHART_NAME}
 EXPECT_CHART=`echo ${GITHUB_REPOSITORY} | awk -F "/" '{print $2}'`
@@ -19,24 +18,30 @@ echo "TARGET_CHART $TARGET_CHART not same as EXPECT_CHART $EXPECT_CHART, please 
 exit 1
 fi
 
-echo "list current common*.image.repository"
-yq r --printMode pv values.yaml "common*.image.repository"
-echo "latest GITLAB_IMAGE_NAME is ${GITLAB_REGISTRY}/${GITHUB_REPOSITORY}"
-yq w -i values.yaml "common*.image.repository" ${GITLAB_REGISTRY}/${GITHUB_REPOSITORY}
-yq r --printMode pv values.yaml "common*.image.repository"
-            
+# echo "list current common*.image.repository"
+# yq r --printMode pv values.yaml "common*.image.repository"
+# echo "latest GITLAB_IMAGE_NAME is ${GITLAB_REGISTRY}/${GITHUB_REPOSITORY}"
+# yq w -i values.yaml "common*.image.repository" ${GITLAB_REGISTRY}/${GITHUB_REPOSITORY}
+# yq r --printMode pv values.yaml "common*.image.repository"
+
+if [ "$SECOND_MODULE" != "false" ];then
+SECOND_MODULE_FOR_COMMON="common2 common3"
+echo ${SECOND_MODULE_FOR_COMMON} | awk  '{print $1}'
+COMMON_FOR_SECOND_MODULE=`echo ${SECOND_MODULE_FOR_COMMON} | awk  '{print $1}'`
+TAG_FOR_SECOND_MODULE=`yq r --printMode pv values.yml ${COMMON_FOR_SECOND_MODULE}.image.tag`
+fi
+
 echo "list current common*.image.tag"
 yq r --printMode pv values.yaml "common*.image.tag"
 echo "latest GITLAB_IMAGE_TAG is ${GITHUB_SHA:0:8}"            
 yq w -i values.yaml "common*.image.tag" ${GITHUB_SHA:0:8}
 yq r --printMode pv values.yaml "common*.image.tag"
 
-if [ "$COMMON2" != "false" ];then
-echo "list common2.image tag"
-yq r --printMode pv values.yaml "common*.image.tag"
+if [ "$SECOND_MODULE" != "false" ];then
+for w in `echo ${SECOND_MODULE_FOR_COMMON}` do yq w -i values.yaml ${w}.image.tag ${TAG_FOR_SECOND_MODULE}; done
 fi
 
-
+yq r --printMode pv values.yaml "common*.image.tag"
 
 echo "push the latest SHA: ${GITHUB_SHA:0:8} to manifest repo"
 git config user.name ${GITHUB_ACTOR}
