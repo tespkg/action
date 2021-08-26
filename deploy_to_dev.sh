ac70d8a80ac9cbdec3b82c2d78edd7bbd6ac6f0f
along8846 <ywl444@126.com>
2021-08-26 14:36:54 +0800

    test

--- deploy_to_dev.sh
+++ deploy_to_dev.sh
@@ -6,6 +6,9 @@ echo "APP_PROJECT_NAME4 is ${APP_PROJECT_NAME4}"
 echo "APP_PROJECT_NAME5 is ${APP_PROJECT_NAME5}"
 echo "GITLAB_REGISTRY is ${GITLAB_REGISTRY}"
 echo "GITHUB_REPOSITORY is ${GITHUB_REPOSITORY}"
+echo "COMMON2 is ${COMMON2}"
+echo "COMMON3 is ${COMMON3}"
+echo "COMMON4 is ${COMMON4}"
 
 cd helm/${APP_PROJECT_NAME}/${APP_CHART_NAME}
 EXPECT_CHART=`echo ${GITHUB_REPOSITORY} | awk -F "/" '{print $2}'`
@@ -27,6 +30,13 @@ yq r --printMode pv values.yaml "common*.image.tag"
 echo "latest GITLAB_IMAGE_TAG is ${GITHUB_SHA:0:8}"            
 yq w -i values.yaml "common*.image.tag" ${GITHUB_SHA:0:8}
 yq r --printMode pv values.yaml "common*.image.tag"
+
+if [ "$COMMON2" != "false" ];then
+echo "list common2.image tag"
+yq r --printMode pv values.yaml "common*.image.tag"
+fi
+
+
 
 echo "push the latest SHA: ${GITHUB_SHA:0:8} to manifest repo"
 git config user.name ${GITHUB_ACTOR}
