eb5df0cbe4dd503ccdc1ae5b435ee8c9eaeafad4
along8846 <ywl444@126.com>
2021-08-26 18:23:33 +0800

    deploy_to_test

--- deploy_to_test.sh
+++ deploy_to_test.sh
@@ -1,6 +1,4 @@
-echo "---------- run deploy_to_test --------------"
-
-ENV=
+echo "---------- run deploy_to_${TES_ENV} --------------"
 
 cd helm/${APP_PROJECT_NAME}
 EXPECT_CHART=`echo ${GITHUB_REPOSITORY} | awk -F "/" '{print $2}'`
@@ -12,30 +10,28 @@ exit 1
 fi
 
 if [ ! -d tmp ]; then mkdir tmp ; fi
-if [ -d "0-env-test/${APP_CHART_NAME}" ];then
-  cp -v ./0-env-test/${APP_CHART_NAME}/values-test*.yaml tmp/
-  rm -rf ./0-env-test/${APP_CHART_NAME}
-  cp -r ${APP_CHART_NAME} 0-env-test/
-  cp -v tmp/values-test*.yaml ./0-env-test/${APP_CHART_NAME}/ && rm -rf tmp
+if [ -d "0-env-${TES_ENV}/${APP_CHART_NAME}" ];then
+  cp -v ./0-env-${TES_ENV}/${APP_CHART_NAME}/values-test*.yaml tmp/
+  rm -rf ./0-env-${TES_ENV}/${APP_CHART_NAME}
+  cp -r ${APP_CHART_NAME} 0-env-${TES_ENV}/
+  cp -v tmp/values-test*.yaml ./0-env-${TES_ENV}/${APP_CHART_NAME}/ && rm -rf tmp
 else 
-  mkdir -p 0-env-test/${APP_CHART_NAME}
-  cp -r ${APP_CHART_NAME} 0-env-test/
-  file="0-env-test/${APP_CHART_NAME}/values-test.yaml"
+  mkdir -p 0-env-${TES_ENV}/${APP_CHART_NAME}
+  cp -r ${APP_CHART_NAME} 0-env-${TES_ENV}/
+  file="0-env-${TES_ENV}/${APP_CHART_NAME}/values-${TES_ENV}.yaml"
   if [ ! -f  $file ];then
-    touch 0-env-test/${APP_CHART_NAME}/values-test.yaml
-
+    touch 0-env-${TES_ENV}/${APP_CHART_NAME}/values-${TES_ENV}.yaml
   fi
 fi
+
 env
-cd 0-env-test
+
+cd 0-env-${TES_ENV}
 IMAGE_TAG=`echo ${GITHUB_REF} | awk -F "/" '{print $3}'`
 if [[ ${IMAGE_TAG} == v* ]]; then IMAGE_TAG=`echo ${IMAGE_TAG:1}`; fi
 echo "replace appVersion and version "
 yq w -i ${APP_CHART_NAME}/Chart.yaml appVersion  --style=double ${IMAGE_TAG}
 yq w -i ${APP_CHART_NAME}/Chart.yaml version  --style=double ${IMAGE_TAG}
-
-
-
 yq w -i ${APP_CHART_NAME}/values.yaml "common*.image.tag" ${IMAGE_TAG}
 
 helmv3 repo add meeraspace ${{ secrets.HELM_REPO_QA }} --username=${{ secrets.HELM_USER }} --password=${{ secrets.HELM_PASSWORD }}
