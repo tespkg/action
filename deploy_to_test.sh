ae6e002ab653cd447d02b70a0512d77872ff2bea
along8846 <ywl444@126.com>
2021-08-31 13:15:29 +0800

    deploy to test

--- deploy_to_test.sh
+++ deploy_to_test.sh
@@ -1,33 +1,28 @@
-echo "---------- run deploy_to_${TES_ENV} --------------"
+echo "---------- run deploy_to_${GITHUB_JOB} --------------"
 
-cd helm/${APP_PROJECT_NAME}
-EXPECT_CHART=`echo ${GITHUB_REPOSITORY} | awk -F "/" '{print $2}'`
-TARGET_CHART=`yq r ${APP_CHART_NAME}/Chart.yaml name`
-echo "verify the EXPECT_CHART:$EXPECT_CHART and TARGET_CHART:$TARGET_CHART"
-if [ "$EXPECT_CHART" != "$TARGET_CHART" ];then
-echo "TARGET_CHART $TARGET_CHART not same as EXPECT_CHART $EXPECT_CHART, please check"
-exit 1
-fi
+echo "ALIAS_GITHUB_REPOSITORY: ${ALIAS_GITHUB_REPOSITORY}"
+APP_CHART_NAME=`echo ${ALIAS_GITHUB_REPOSITORY} | awk -F "/" '{print $2}'`
+echo "APP_CHART_NAME: ${APP_CHART_NAME}"
 
-if [ ! -d tmp ]; then mkdir tmp ; fi
-if [ -d "0-env-${TES_ENV}/${APP_CHART_NAME}" ];then
-  cp -v ./0-env-${TES_ENV}/${APP_CHART_NAME}/values-test*.yaml tmp/
-  rm -rf ./0-env-${TES_ENV}/${APP_CHART_NAME}
-  cp -r ${APP_CHART_NAME} 0-env-${TES_ENV}/
-  cp -v tmp/values-test*.yaml ./0-env-${TES_ENV}/${APP_CHART_NAME}/ && rm -rf tmp
+echo "SECOND_MODULE is ${SECOND_MODULE}"
+echo "THIRD_MODULE is ${THIRD_MODULE}"
+
+if [ -d "env-${TES_ENV}/${APP_CHART_NAME}" ];then
+  if [ ! -d tmp ]; then mkdir tmp ; fi
+  cp -v env-${TES_ENV}/${APP_CHART_NAME}/values-test*.yaml tmp/
+  rm -rf env-${TES_ENV}/${APP_CHART_NAME}
+  cp -r env-${TES_ENV}/${APP_CHART_NAME} env-${TES_ENV}/
+  cp -v tmp/values-test*.yaml env-${TES_ENV}/${APP_CHART_NAME}/ && rm -rf tmp
 else 
-  mkdir -p 0-env-${TES_ENV}/${APP_CHART_NAME}
-  cp -r ${APP_CHART_NAME} 0-env-${TES_ENV}/
-  file="0-env-${TES_ENV}/${APP_CHART_NAME}/values-${TES_ENV}.yaml"
-  if [ ! -f  $file ];then
-    touch 0-env-${TES_ENV}/${APP_CHART_NAME}/values-${TES_ENV}.yaml
-  fi
+  echo "mkdir -p env-${TES_ENV}/${APP_CHART_NAME}"
+  mkdir -p env-${TES_ENV}/${APP_CHART_NAME}
+  cp -r env-dev/${APP_CHART_NAME} env-${TES_ENV}/
+  file="env-${TES_ENV}/${APP_CHART_NAME}/values-${TES_ENV}.yaml"
+  if [ ! -f  $file ];then touch env-${TES_ENV}/${APP_CHART_NAME}/values-${TES_ENV}.yaml fi
 fi
 
-# env
-
-echo "------cd 0-env-${TES_ENV}/${APP_CHART_NAME} -----"
-cd 0-env-${TES_ENV}/${APP_CHART_NAME}
+echo "------cd env-${TES_ENV}/${APP_CHART_NAME} -----"
+cd env-${TES_ENV}/${APP_CHART_NAME}
 
 IMAGE_TAG=`echo ${GITHUB_REF} | awk -F "/" '{print $3}'`
 if [[ ${IMAGE_TAG} == v* ]]; then IMAGE_TAG=`echo ${IMAGE_TAG:1}`; fi
