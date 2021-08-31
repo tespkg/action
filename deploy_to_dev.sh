10e276b9c9bc4361a0dfc7112037fc23382d4361
along8846 <ywl444@126.com>
2021-08-31 10:48:44 +0800

    dev

--- deploy_to_dev.sh
+++ deploy_to_dev.sh
@@ -1,15 +1,16 @@
 echo "------ run deploy_to_dev ------"
 
+echo "ALIAS_GITHUB_REPOSITORY: ${ALIAS_GITHUB_REPOSITORY}"
+APP_CHART_NAME=`echo ${ALIAS_GITHUB_REPOSITORY} | awk -F "/" '{print $2}'`
+echo "APP_CHART_NAME: ${APP_CHART_NAME}"
+
 echo "SECOND_MODULE is ${SECOND_MODULE}"
 echo "THIRD_MODULE is ${THIRD_MODULE}"
 
-cd helm/${APP_PROJECT_NAME}/${APP_CHART_NAME}
-EXPECT_CHART=`echo ${GITHUB_REPOSITORY} | awk -F "/" '{print $2}'`
-TARGET_CHART=`yq r Chart.yaml name`
-echo "verify the EXPECT_CHART: $EXPECT_CHART and TARGET_CHART: $TARGET_CHART"
-if [ "$EXPECT_CHART" != "$TARGET_CHART" ];then
-echo "TARGET_CHART $TARGET_CHART not same as EXPECT_CHART $EXPECT_CHART, please check"
-exit 1
+cd env-dev/${APP_CHART_NAME}
+if [ $? != 0 ]; then
+    echo "err, no such directory env-dev/${APP_CHART_NAME} "
+    exit 1
 fi
 
 if [ "$SECOND_MODULE" == "ignore" ] ;then
@@ -21,7 +22,7 @@ TAG_FOR_SECOND_MODULE=`yq r values.yaml ${COMMON_FOR_SECOND_MODULE}.image.tag`
 fi
 
 if [ "$THIRD_MODULE" == "ignore" ] ;then
-echo "------ Reserved image for ${THIRD_MODULE_FOR_COMMON} due to THIRD_MODULE='ingore------"
+echo "------ Reserved image for ${THIRD_MODULE_FOR_COMMON} due to THIRD_MODULE=ingore   ------"
 COMMON_FOR_THIRD_MODULE=`echo ${THIRD_MODULE_FOR_COMMON} | awk  '{print $1}'`
 echo "------ Name of the image.repo to be reserved:  `yq r values.yaml ${COMMON_FOR_THIRD_MODULE}.image.repository`"
 echo "------ Name of the image.tag to be reserved:   `yq r values.yaml ${COMMON_FOR_THIRD_MODULE}.image.tag`"
@@ -53,11 +54,11 @@ fi
 echo "------ list latest common*.image.tag ------"
 yq r --printMode pv values.yaml "common*.image.tag"
 
-echo "push the latest SHA: ${GITHUB_SHA:0:8} to manifest repo"
+echo "push the latest SHA: ${GITHUB_SHA:0:8} to the manifest repo path: env-dev/${APP_CHART_NAME}"
 git config user.name ${GITHUB_ACTOR}
 git config user.email ${GITHUB_ACTOR}@github.com
 git diff
 git add ./
 git commit -m "${GITHUB_REPOSITORY}_${GITHUB_JOB}_${GITHUB_SHA:0:8}_details:${CI_COMMIT_MESSAGE}"
 git push
-echo -e "======================= \n\n you can check your application status with 'user/passwd:readonly/Te****g' at \n\n \033[31m https://g-argocd.fluxble.com/applications/${APP_PROJECT_NAME}-${APP_CHART_NAME}\033[0m \n\n======================="
+echo -e "======================= \n\n you can check your application status with 'user/passwd:readonly/Te****g' at \n\n \033[31m https://g-argocd.fluxble.com/applications/${APP_CHART_NAME}\033[0m \n\n======================="
