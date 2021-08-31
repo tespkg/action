433c765ca1880494c19c47c4335a588827d16d92
along8846 <ywl444@126.com>
2021-08-31 17:20:48 +0800

    test

--- install_yq_helm.sh
+++
@@ -1,5 +0,0 @@
-echo "set up"
-wget https://github.com/mikefarah/yq/releases/download/3.4.1/yq_linux_amd64 -O /usr/bin/yq && sudo chmod +x /usr/bin/yq
-wget -q https://get.helm.sh/helm-v3.1.1-linux-amd64.tar.gz -O - | tar -xzO linux-amd64/helm > /usr/local/bin/helmv3 && chmod +x /usr/local/bin/helmv3
-yq -V
-helmv3 version
