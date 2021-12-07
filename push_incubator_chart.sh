333dcdf34ecad0a7d07f91630562e7c5e25d1941
weilong <weilong.yi@target-energysolutions.com>
2021-12-07 15:43:19 +0800

    push_incubator_chart.sh

--- push_incubator_chart.sh
+++ push_incubator_chart.sh
@@ -10,9 +10,12 @@ echo "FOURTH_MODULE is ${FOURTH_MODULE}"
 IMAGE_TAG=`echo ${GITHUB_REF} | awk -F "/" '{print $3}'`
 
 if [[ ${IMAGE_TAG} =~ "mixedmanual" ]]; then
+  if [ ! -d "env-mixed/${APP_CHART_NAME}-${GITHUB_REF_NAME}" ];then
+    cp -r env-dev/${APP_CHART_NAME} env-mixed/${APP_CHART_NAME}-${GITHUB_REF_NAME} || exit 1
+    echo "cp -r env-dev/${APP_CHART_NAME} env-mixed/${APP_CHART_NAME}-${GITHUB_REF_NAME}" 
+  fi
   echo "run non-Standard deployment"
   echo "cd  env-mixed/${APP_CHART_NAME}-${BRANCH_NAME}"
-  cd env-mixed/${APP_CHART_NAME}-${BRANCH_NAME} || cp -r env-dev/${APP_CHART_NAME} env-mixed/${APP_CHART_NAME}-${BRANCH_NAME} || exit 1
 else
   echo "run Standard deployment"
   echo "cd env-${TES_ENV}/${APP_CHART_NAME}"
