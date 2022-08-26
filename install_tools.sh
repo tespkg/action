eb48249402d71227cc7dd1128c1302d48f39919c
Shaojun Wu <83997493+shaojun-tes@users.noreply.github.com>
2022-08-26 10:37:40 +0800

    Update install_tools.sh
    add echo"pwd"

--- install_tools.sh
+++ install_tools.sh
@@ -1,4 +1,7 @@
 echo "set up"
+echo 'pwd:'
+pwd
+ls -lah
 # wget -q https://github.com/mikefarah/yq/releases/download/3.4.1/yq_linux_amd64 -O /usr/bin/yq && sudo chmod +x /usr/bin/yq
 # wget -q https://get.helm.sh/helm-v3.1.1-linux-amd64.tar.gz -O - | tar -xzO linux-amd64/helm > /usr/local/bin/helmv3 && chmod +x /usr/local/bin/helmv3
 # yq -V
